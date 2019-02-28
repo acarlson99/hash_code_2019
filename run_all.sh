cee4768976ac49aa7ba317d79eaf385a0aabe465
Clayton Allen <callen@student.42.us.org>
2019-02-28 12:39:06 -0800

    fixed a few typos

--- run_all.sh
+++ run_all.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
-echo 'Running input/a_example.txt'
+echo 'Running a_example.txt'
 ./main.py input/a_example.txt
 mv the_answer.txt a_answer.txt
 
-echo 'Running vely_landscapes.txt'
+echo 'Running b_lovely_landscapes.txt'
 ./main.py input/b_lovely_landscapes.txt
 mv the_answer.txt b_answer.txt
 
-echo 'Running morable_moments.txt'
+echo 'Running c_memorable_moments.txt'
 ./main.py input/c_memorable_moments.txt
 mv the_answer.txt c_answer.txt
 
-echo 'Running /d_pet_pictures.txt'
+echo 'Running d_pet_pictures.txt'
 ./main.py input/d_pet_pictures.txt
 mv the_answer.txt d_answer.txt
 
