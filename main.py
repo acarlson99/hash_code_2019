0c4904f5fd143c6626de5092b971f3c08282130a
Armando Herrera <arherrer@e1z1r2p34.42.us.org>
2019-02-28 10:08:45 -0800

    the parser

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+
+def read_file(path):
+    lines = []
+    with open(path, 'r') as f:
+        lines = f.read().splitlines()
+    num_of_pics = int(lines[0][0])
+    pics = [line.split() for line in lines[1:]]
+    pics = [(pic[0], int(pic[1]), pic[2:]) for pic in pics]
+    return (num_of_pics, pics)
+
+def main(path):
+    num_of_pics, pics = read_file(path)
+    print(num_of_pics, pics)
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 2):
+        main(sys.argv[1])
+
