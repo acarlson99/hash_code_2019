199b8fcdb01bc98989985485e4b36497f6de6a66
Alexander Carlson <acarlson@e1z1r2p32.42.us.org>
2019-02-28 12:02:53 -0800

    Added run_all and updated gitignore

---
+++ run_all.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+./main.py input/a_example.txt
+mv the_answer.txt a_answer.txt
+
+./main.py input/b_lovely_landscapes.txt
+mv the_answer.txt b_answer.txt
+
+./main.py input/c_memorable_moments.txt
+mv the_answer.txt c_answer.txt
+
+./main.py input/d_pet_pictures.txt
+mv the_answer.txt d_answer.txt
+
+./main.py input/e_shiny_selfies.txt
+mv the_answer.txt e_answer.txt
