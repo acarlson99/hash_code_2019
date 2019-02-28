d693d382b0e5f798cf86393fcbc919be8be54d74
Armando Herrera <arherrer@e1z1r2p34.42.us.org>
2019-02-28 10:18:50 -0800

    indexing

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import sys
 
+################################################################################
+
 def read_file(path):
     lines = []
     with open(path, 'r') as f:
         lines = f.read().splitlines()
     num_of_pics = int(lines[0][0])
     pics = [line.split() for line in lines[1:]]
-    pics = [(pic[0], int(pic[1]), pic[2:]) for pic in pics]
+    pics = [(idx, pic[0], int(pic[1]), pic[2:]) for idx, pic in enumerate(pics)]
     return (num_of_pics, pics)
+
+################################################################################
 
 def main(path):
     num_of_pics, pics = read_file(path)
