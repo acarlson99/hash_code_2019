fc5ef1240fc1bc3765fd0da04197d7ffe8ac5f1e
Alexander Carlson <acarlson@e1z1r2p32.42.us.org>
2019-02-28 10:32:49 -0800

    Added score function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import sys
 
 ################################################################################
@@ -13,8 +14,15 @@ def read_file(path):
 
 ################################################################################
 
+def score(p1, p2):
+    s1 = len(list(set(p1[3]).intersection(p2[3])))
+    s2 = len(np.setdiff1d(p1[3], p2[3]))
+    s3 = len(np.setdiff1d(p2[3], p1[3]))
+    return (min(s1, s2, s3))
+
 def main(path):
     num_of_pics, pics = read_file(path)
+    print(score(pics[0], pics[1]))
     print(num_of_pics, pics)
 
 if __name__ == '__main__':
