015d56883a79066ffa5871d6d1ce745b25cb9097
Alexander Carlson <acarlson@e1z1r2p32.42.us.org>
2019-02-28 12:21:55 -0800

    Added msgs

--- run_all.sh
+++ run_all.sh
@@ -1,16 +1,21 @@
 #! /bin/bash
 
+echo 'Running input/a_example.txt'
 ./main.py input/a_example.txt
 mv the_answer.txt a_answer.txt
 
+echo 'Running vely_landscapes.txt'
 ./main.py input/b_lovely_landscapes.txt
 mv the_answer.txt b_answer.txt
 
+echo 'Running morable_moments.txt'
 ./main.py input/c_memorable_moments.txt
 mv the_answer.txt c_answer.txt
 
+echo 'Running /d_pet_pictures.txt'
 ./main.py input/d_pet_pictures.txt
 mv the_answer.txt d_answer.txt
 
+echo 'Running e_shiny_selfies.txt'
 ./main.py input/e_shiny_selfies.txt
 mv the_answer.txt e_answer.txt
